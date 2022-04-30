1e9d72ac3319ee66851551104abb5e2ac73c0a60
carlos-arman <minerocarlos28@gmail.com>
2022-04-30 13:09:37 -0700

    EJEMPLO 1 TERMINADO

---
+++ EJEMPLOS SEMANA 5/EstructurasDeControl/src/estructuras/ciclicas/EstructuraWhile.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras.ciclicas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Armando
+ */
+public class EstructuraWhile {
+    
+    public static void main(String[] args) {
+        try{
+            Scanner leer = new Scanner(System.in);
+            System.out.println("Ingresar un numero limite de repeticion:");
+            String limite = leer.nextLine();
+            if(limite == null){
+                System.out.println("Dato invalido!");
+            }else {
+                instruccionWhileSencillo(Integer.parseInt(limite));
+            }
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void instruccionWhileSencillo(int valorLimite){
+        int i = 1;
+        while(i<=valorLimite){
+            System.out.println("El valor de la repeticion es: " + i);
+            i++;
+        } 
+    }
+}
