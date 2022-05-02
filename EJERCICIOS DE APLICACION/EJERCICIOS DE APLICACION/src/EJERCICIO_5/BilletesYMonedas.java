37b3fdc6195df4bf7fedbd035683d3d48ca912a4
carlos-arman <minerocarlos28@gmail.com>
2022-05-02 08:09:40 -0700

    REALIZANDO EJERCICIO 5 Armando Minero

---
+++ EJERCICIOS DE APLICACION/EJERCICIOS DE APLICACION/src/EJERCICIO_5/BilletesYMonedas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*Una  agencia  de llamadas  necesita  saber  el  precio  por  las llamadas  
+telefónicas  realizadas por sus clientes y así poder calcular el monto a pagar.
+Para ello se solicita realizar un programaen  Java  que  determine  el  monto  
+a  pagar  por  una  llamada  telefónica  y  que  luego  diga  la cantidad de 
+billetes y monedas a pagar por el cliente teniendo en cuenta lo siguiente:Una 
+llamada  que  dura  hasta  3  minutos  tiene  un  costo  de20  centavos,  por 
+cada  minuto adicional que dura la llamada se le sumará un monto de 0.5 centavos 
+adicionales, por lo tanto se debe leer el tiempo de la llamada.El programa deberá
+de imprimir el número de billetes y de monedas que el usuario debe de pagar por
+el servicio de la llamada realizada. Por ejemplo si el usuario tiene que pagar
+$15.50 Módulo:Metodologías ágiles para el desarrollo de software  --  Facilitador:Manuel de Jesús Gámez López. 
+12el  programa  deberá  de  imprimir  lo  siguiente:  1  billete  de  diez  dólares,
+
+un  billete  de  cinco dólares y dos monedas de veinticinco centavos.Monedas: 1, 5,10 y 25 
+centavos Billetes: 1, 5, 10, 20 dólares Considere que debe darse la menor cantidad de billetes y monedas posibles.
+El nombre de la clase creada será: BilletesYMonedas. */
+
+public class BilletesYMonedas {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+    }
+}
